3893b1e7169435070447461276ace11f1414a01a
Luke Armstrong <me@lukearmstrong.co.uk>
2015-01-08 17:39:34 +0000

    point to birmingham spreadsheet

--- js/main.js
+++ js/main.js
@@ -1,56 +1,59 @@
 $(function (){
-	//load spreadsheet
-	var mySpreadsheet = 'https://docs.google.com/spreadsheet/pub?key=0AgYVWMxsntVodGlNeS13M0V1dmFmakJENTlaMWoxV2c&single=true&gid=3';
-	$('#freelancerslist').sheetrock({
-		url: mySpreadsheet,
-		chunkSize: 100,
-		sql: 'select B,E,C,D,F,G order by A desc',
-		userCallback: function() {
-			links();
-			twitter();
-			websiteLink();
-			tableFilterApplication();
-		}
-	});
+    //load spreadsheet
+    var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1r8eOnzNUdPiFMvzM1LQfhFDMFpI2fWwUH01xDBvA3f0/edit#gid=1338714353';
 
-	// work out portfolio link
-	function websiteLink() {
-		$('table td a:not([href*="twitter"])').parent().addClass('website-link');
-	}
-
-	// add a table filter
-	function tableFilterApplication() {
-		$('table').filterTable({
-			quickList: ['Branding', 'Interaction design', 'User experience', 'Front-end', 'Copywriting'],
-			placeholder: "Search this list"
-			
-	        });
-	}	
-
-	// add boostrap style to table
-	$('table').addClass('table table-striped table-hover');
-
-
-	//add links
-	function links() {
-		var websiteEl = $('#freelancerslist td:nth-child(5)');
-		websiteEl.each(function(index) {
-			var link = $(this).text();
-			$(this).wrapInner('<a target="_blank" href="'+ link +'" />');
-		});	
-	};
-
-	//add links
-	function twitter() {
-		var twitterEl = $('#freelancerslist td:nth-child(6)');
-		twitterEl.each(function(index) {
-			var link = "http://twitter.com/"; //twitter url 
-			var text = $(this).text();
-				text = text.replace("@", "");
-			link += text;
-			$(this).wrapInner('<a target="_blank" href="'+ link +'" />');
-		});	
-	}
-
-
+    $('#freelancerslist').sheetrock({
+        url: mySpreadsheet,
+        chunkSize: 100,
+        sql: 'select B,C,D,F,E,H,G,I order by A desc',
+        userCallback: function() {
+            links();
+            twitter();
+            birminghamIO();
+            tableFilterApplication();
+        }
+    });
 });
+
+// Add Website Links
+function links() {
+    var websiteEl = $('#freelancerslist td:nth-child(6)');
+    websiteEl.each(function(index) {
+        var link = $(this).text();
+        $(this).wrapInner('<a target="_blank" href="'+ link +'" />');
+    });
+}
+
+// Add Twitter links
+function twitter() {
+    var twitterEl = $('#freelancerslist td:nth-child(7)');
+    twitterEl.each(function(index) {
+        var link = "http://twitter.com/";
+        var text = $(this).text();
+            text = text.replace("@", "");
+        link += text;
+        $(this).wrapInner('<a target="_blank" href="'+ link +'" />');
+    });
+}
+
+// Add Birmingham.IO Links
+function birminghamIO() {
+    var twitterEl = $('#freelancerslist td:nth-child(8)');
+    twitterEl.each(function(index) {
+        var link = "https://talk.birmingham.io/users/";
+            link += $(this).text();
+
+        $(this).wrapInner('<a target="_blank" href="'+ link +'" />');
+    });
+}
+
+// Add a table filter
+function tableFilterApplication() {
+    $('#freelancerslist').filterTable({
+        quickList: ['Branding', 'Graphic Design', 'JavaScript', 'PHP'],
+        placeholder: "Search this list"
+    });
+
+     // add boostrap style to table
+    $('#freelancerslist').addClass('table table-striped table-hover');
+}
